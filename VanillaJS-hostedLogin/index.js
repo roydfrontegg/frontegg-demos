09054d12f0c0f3ce9ba5ba5bea2dda1f3a8fbbe6
Roy Daniel <roy.daniel@frontegg.com>
2022-12-20 17:36:59 -0500

    fix

--- VanillaJS-hostedLogin/index.js
+++ VanillaJS-hostedLogin/index.js
@@ -8,45 +8,45 @@ document.getElementsByTagName('head')[0].appendChild(style);
 
 const app = initialize({
   contextOptions: {
-    baseUrl: "https://app-frtqiefxjqn9.frontegg.com", //set your Frontegg environment domain and client ID here
+    baseUrl: 'https://app-frtqiefxjqn9.frontegg.com', //set your Frontegg environment domain and client ID here
     clientId: '04017595-4e5d-4e7e-aff6-93c58d489d2f'
   },
   hostedLoginBox: true
 })
 
-document.getElementById("loginWithRedirect").addEventListener('click', () => {
-    app.loginWithRedirect()
-  })
-  
-  document.getElementById("logout").addEventListener('click', () => {
-    app.logout()
-  })
-  
-  app.store.subscribe(() => {
-    const state = app.store.getState();
-    if (state.auth.user) {
-      document.getElementById('user-container').innerText = state.auth.user.email;
-    } else {
-      document.getElementById('user-container').innerText = 'Not Authenticated'
-    }
-  
-    document.getElementById('app-root').style.display = state.auth.isLoading ? 'hidden' : 'block'
-  
-  
-    let styleHtml = ''
-    if (state.auth.isAuthenticated) {
-      styleHtml += '[fe-state="isAuthenticated"] { }';
-      styleHtml += '[fe-state="!isAuthenticated"] { display: none; }';
-    } else {
-      styleHtml += '[fe-state="isAuthenticated"] { display: none; }';
-      styleHtml += '[fe-state="!isAuthenticated"] { }';
-    }
-  
-    if(app.options.hostedLoginBox){
-      styleHtml += '[fe-mode="hosted"] { }';
-    } else {
-      styleHtml += '[fe-mode="hosted"] { display: none; }';
-    }
-  
-    style.innerHTML = styleHtml;
-  })
+document.getElementById("loginWithRedirect").addEventListener("click", () => {
+  app.loginWithRedirect()
+})
+
+document.getElementById("logout").addEventListener("click", () => {
+  app.logout()
+})
+
+app.store.subscribe(() => {
+  const state = app.store.getState();
+  if (state.auth.user) {
+    document.getElementById('user-container').innerText = state.auth.user.email;
+  } else {
+    document.getElementById('user-container').innerText = 'Not Authenticated'
+  }
+
+  document.getElementById('app-root').style.display = state.auth.isLoading ? 'hidden' : 'block'
+
+
+  let styleHtml = ``
+  if (state.auth.isAuthenticated) {
+    styleHtml += `[fe-state="isAuthenticated"] { }`;
+    styleHtml += `[fe-state="!isAuthenticated"] { display: none; }`;
+  } else {
+    styleHtml += `[fe-state="isAuthenticated"] { display: none; }`;
+    styleHtml += `[fe-state="!isAuthenticated"] { }`;
+  }
+
+  if(app.options.hostedLoginBox){
+    styleHtml += `[fe-mode="hosted"] { }`;
+  } else {
+    styleHtml += `[fe-mode="hosted"] { display: none; }`;
+  }
+
+  style.innerHTML = styleHtml;
+})
